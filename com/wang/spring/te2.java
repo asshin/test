ba33c021084d623a1599f35267b472e3082ac60e
wang <857541619@qq.com>
2021-10-31 15:17:05 +0800

    thrid commit

--- com/wang/spring/te2.java
+++ com/wang/spring/te2.java
@@ -8,5 +8,7 @@ public class te2 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
